9d2cbfb7e2a2bce2702c186d686ff7b216f006b1
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-07-23 17:18:42 +1000

    Initial setup

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="./public/styles.css" rel="stylesheet">
+    <title>E-Trading Post</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>E-Trading Post</h1>
+            <img src="./public/assets/bars-solid.svg" class="hamb-menu">
+            <div id="hamb-links">
+                <a>Explore</a>
+                <a>Sell</a>
+                <a>My Account</a>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
