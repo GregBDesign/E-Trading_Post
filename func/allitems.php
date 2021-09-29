f305db8e4a6f426a9586f7b2676aa323fd0ce6b1
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-09-29 15:50:43 +1000

    Updates

--- func/allitems.php
+++ func/allitems.php
@@ -1,5 +1,5 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/inc/conn.php');
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/inc/connheroku.php');
 
     try {
         $retrieveItems = "SELECT * FROM item 
