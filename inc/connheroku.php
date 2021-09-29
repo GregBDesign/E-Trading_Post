c5b5a1cfd7b9eece99b1feed61e9575b154d8c3d
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-09-29 14:45:44 +1000

    Adds Heroku DB config

---
+++ inc/connheroku.php
@@ -0,0 +1,17 @@
+<?php 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$db", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "A database error has occured";
+    echo $e;
+}
+
+?>
